bcd2c98bb7b77578b30bc7e99aa0a94e2830e275
Ewa Marciniak <emarciniak78@gmail.com>
2013-12-05 00:24:49 +0000

    Few more changes to speed things up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,23 +11,17 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
-//= require jquery.ui.accordion
 //= require jquery_ujs
 //= require jquery.ui.datepicker
+//= require jquery.ui.accordion
 //= require raphael
 //= require morris
 //= require pwstrength
 //= require chosen.jquery
 //=require foundation/foundation
-//=require foundation/foundation.alerts
-//=require foundation/foundation.clearing
-//=require foundation/foundation.dropdown
 //=require foundation/foundation.forms
-//=require foundation/foundation.reveal
-//=require foundation/foundation.section
 //=require foundation/foundation.tooltips
 //=require foundation/foundation.topbar
-//=require foundation/foundation.interchange
 //=require foundation/foundation.placeholder
 //= require_tree .
 
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ActiveRecord::Base
   attr_accessible :calories_per_portion, :cooking_time, :directions, :integer, \
   :number_of_portions, :preparation_time, :shopping_list, :text, :title, :ingredient_ids, :picture
   has_and_belongs_to_many :ingredients
-  has_attached_file :picture, :styles => { :large => "500x500>", :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :picture, :styles => { :large => "500x500>", :medium => "250x250>", :thumb => "100x100>" }
   validates_attachment :picture, :presence => true,
                        :size => {:in => 0..500.kilobytes}
 
